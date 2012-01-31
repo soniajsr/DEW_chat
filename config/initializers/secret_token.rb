3a11256f332debd2d09114530597423f63025b67
Janet <sonia.jsr@gmail.com>
2012-01-30 21:45:27 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dewchat::Application.config.secret_token = '312bff842ea53456c243c9b549521bb30f53b6fb09c295ba32969cd2ec9dd4ed76895871bf6f01523f7861e50a6e8fdae08028d31a6fd6c7eb0b8c39575a64ba'
